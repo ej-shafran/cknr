7aa2ae6ae31d9ccd35056829627f800111951737
ej-shafran <ejshafran@hilma.tech>
2023-07-04 10:17:56 +0300

    Exercise 1-4

--- conversion.c
+++ conversion.c
@@ -2,17 +2,23 @@
 
 int main(void)
 {
-	float celsius;
 	int lower, upper, step;
 
 	lower = 0;
 	upper = 300;
 	step = 20;
 
-	printf("FAHR  CELSIUS\n\n");
+	printf("FAHR   CELSIUS\n\n");
 
-	for (float fahr = lower; fahr <= upper; fahr += step) {
-		celsius = ((fahr * 5.0) - 160.0) / 9.0;
-		printf("%4.0f %8.1f\n", fahr, celsius);
+	for (float fahr = 0; fahr <= 300; fahr += 20) {
+		float celsius = ((fahr * 5.0) - 160.0) / 9.0;
+		printf("%4.0f  %8.1f\n", fahr, celsius);
+	}
+
+	printf("\nCELSIUS   FAHR\n\n");
+
+	for (float celsius = 0; celsius <= 150; celsius += 10) {
+		float fahr = ((celsius * 9.0) + 160.0) / 5.0;
+		printf("%7.0f  %5.1f\n", celsius, fahr);
 	}
 }
