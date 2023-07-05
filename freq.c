5bfe4a57e28ec7f19e1b653f981df853b09f0532
ej-shafran <ejshafran@hilma.tech>
2023-07-05 16:27:00 +0300

    Exercise 1-14

---
+++ freq.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define UPPER 25 /* number of lowercase letters */
+
+int main(void)
+{
+	char c;
+
+	int letters[UPPER];
+
+	for (int i = 0; i < UPPER; i++)
+		letters[i] = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c >= 'a' && c <= 'z')
+			++letters[c - 'a'];
+	}
+
+	int max = 0;
+	for (int i = 0; i < UPPER; i++) {
+		if (letters[i] > max)
+			max = letters[i];
+	}
+
+	for (int i = max; i > 0; i--) {
+		printf("%2d ", i);
+		for (int j = 0; j < UPPER; j++) {
+			if (letters[j] >= i)
+				printf(" | ");
+			else
+				printf("   ");
+		}
+		printf("\n");
+	}
+
+	printf("   ");
+	for (int i = 0; i <= UPPER; i++) {
+		printf("%2c ", i + 'a');
+	}
+}
